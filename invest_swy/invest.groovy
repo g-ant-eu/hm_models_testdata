b3b0a3e4a2dc28d61876d2be056e58c71ee406cc
Andrea Antonello <andrea.antonello@gmail.com>
2025-11-01 05:24:02 +0100

    add whole year data

--- invest_swy/invest.groovy
+++ invest_swy/invest.groovy
@@ -3,17 +3,20 @@ import org.hortonmachine.*
 
 def baseFolder = "/home/hydrologis/development/hm_models_testdata/invest_swy/"
 
+def month = 4
+def monthStr = "${month-1}"
+
 def quickflowInputsFolder = baseFolder + "/01_input_quickflow/"
 def quickflowOutputsFolder = baseFolder + "/01_output_quickflow/"
 if( !new File(quickflowOutputsFolder).exists() ){
     new File(quickflowOutputsFolder).mkdirs()
 }
 
-def rainMap = quickflowInputsFolder + "prcp_a0.tif"
+def rainMap = quickflowInputsFolder + "prcp_a"+monthStr+".tif"
 def netMap = quickflowInputsFolder + "hm_denet1000_gura.tif"
 def cnMap = quickflowInputsFolder + "CN.tif"
-def eventsMap = quickflowInputsFolder + "n_events0.tif"
-def quickflowMap = quickflowOutputsFolder + "hm_quickflow.tif"
+def eventsMap = quickflowInputsFolder + "n_events"+monthStr+".tif"
+def quickflowMap = quickflowOutputsFolder + "hm_quickflow"+monthStr+".tif"
 
 def petInputFolder = baseFolder + "02_input_pet/"
 def petOutputFolder = baseFolder + "02_output_pet/"
@@ -21,9 +24,9 @@ if( !new File(petOutputFolder).exists() ){
     new File(petOutputFolder).mkdirs()
 }
 
-def referenceEtpMap = petInputFolder + "et0_a0.tif"
-def kcMap = petInputFolder + "kc_0.tif"
-def petMap = petOutputFolder + "hm_pet.tif"
+def referenceEtpMap = petInputFolder + "et0_a"+monthStr+".tif"
+def kcMap = petInputFolder + "kc_"+monthStr+".tif"
+def petMap = petOutputFolder + "hm_pet"+monthStr+".tif"
 
 def rechargeInputFolder = baseFolder + "03_input_recharge/"
 def rechargeOutputFolder = baseFolder + "03_output_recharge/"
@@ -32,10 +35,10 @@ if( !new File(rechargeOutputFolder).exists() ){
 }
 
 def flowMap = rechargeInputFolder + "hm_dedrain_gura.tif"
-def infMap = rechargeOutputFolder + "hm_infiltration.tif"
-def netInfMap = rechargeOutputFolder + "hm_netinfiltration.tif"
-def aetMap = rechargeOutputFolder + "hm_aet.tif"
-def lsumAvailableMap = rechargeOutputFolder + "hm_lsum_available.tif"
+def rechargeMap = rechargeOutputFolder + "hm_recharge"+monthStr+".tif"
+def availableRechargeMap = rechargeOutputFolder + "hm_available_recharge"+monthStr+".tif"
+def aetMap = rechargeOutputFolder + "hm_aet"+monthStr+".tif"
+def upslopeSubsidyMap = rechargeOutputFolder + "hm_upslopeSubsidy"+monthStr+".tif"
 def alpha = 1.0/12.0
 
 def baseflowInputsFolder = baseFolder + "04_input_baseflow/"
@@ -44,17 +47,17 @@ if( !new File(baseflowOutputFolder).exists() ){
     new File(baseflowOutputFolder).mkdirs()
 }
 
-def bfMap = baseflowOutputFolder + "hm_baseflow.tif"
-def lsumMap = baseflowOutputFolder + "hm_lsum.tif"
-def vriMap = baseflowOutputFolder + "hm_vri.tif"
-def biMap = baseflowOutputFolder + "hm_b.tif"
+def bfMap = baseflowOutputFolder + "hm_baseflow"+monthStr+".tif"
+def lsumMap = baseflowOutputFolder + "hm_lsum"+monthStr+".tif"
+def vriMap = baseflowOutputFolder + "hm_vri"+monthStr+".tif"
+def biMap = baseflowOutputFolder + "hm_b"+monthStr+".tif"
 
 //println HM.printColorTables()
 
 def doScs = true
 def doPet = true
-def doInf = false
-def doBaseFlow = false
+def doRech = true
+def doBaseFlow = true
 
 
 //HM.makeQgisStyleForRaster("flow", flowMap, 3)
@@ -84,29 +87,29 @@ if(doPet){
     HM.makeQgisStyleForRaster("blues", petMap, 3)
 }
 
-if(doInf){
+if(doRech){
     def v = new SWYRechargeRouting()
     v.pAlpha = alpha
     v.inPet = petMap
     v.inFlowdirections = flowMap
     v.inNet = netMap
-    v.inRainfall = rainMap
-    v.inRunoff = quickflowMap
-    v.outInfiltration = infMap
-    v.outNetInfiltration = netInfMap
+    v.inPrecipitation = rainMap
+    v.inQuickflow = quickflowMap
     v.outAet = aetMap
-    v.outLsumAvailable = lsumAvailableMap
+    v.outRecharge = rechargeMap
+    v.outUpslopeSubsidy = upslopeSubsidyMap
+    v.outAvailableRecharge = availableRechargeMap
     v.process()
-    HM.makeQgisStyleForRaster("blues", infMap, 3)
-    HM.makeQgisStyleForRaster("blues", netInfMap, 3)
+    HM.makeQgisStyleForRaster("blues", rechargeMap, 3)
+    HM.makeQgisStyleForRaster("blues", availableRechargeMap, 3)
     HM.makeQgisStyleForRaster("reds", aetMap, 3)
-    HM.makeQgisStyleForRaster("violets", lsumMap, 3)
+    HM.makeQgisStyleForRaster("violets", upslopeSubsidyMap, 3)
 }
 
 if(doBaseFlow){
-    def b = new BaseflowWaterVolume()
-    b.inInfiltration = infMap
-    b.inNetInfiltration = netInfMap
+    def b = new SWYBaseflowRouting()
+    b.inRecharge = rechargeMap
+    b.inAvailableRecharge = availableRechargeMap
     b.inNet = netMap
     b.inFlowdirections = flowMap
     b.outBaseflow = bfMap
